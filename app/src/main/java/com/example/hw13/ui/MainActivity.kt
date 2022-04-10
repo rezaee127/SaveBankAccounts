6c7f98abfa5ed34bf04da91a747555f34e729a70
razi <rezaie127@yahoo.com>
2022-04-11 00:46:37 +0430

    add navigation drawer

--- app/src/main/java/com/example/hw13/ui/MainActivity.kt
+++ app/src/main/java/com/example/hw13/ui/MainActivity.kt
@@ -2,16 +2,54 @@ package com.example.hw13.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hw13.R
 import com.example.hw13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
 
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.profileFragment,
+            R.id.createAccountFragment,R.id.selectAccountFragment,R.id.showAccountFragment), binding.drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
+    }
+
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+    }
+    fun onNavigationItemSelected( item: MenuItem){
+       // if (item.itemId==R.id.exit){
+            finishAffinity()
+       // }
     }
 }
